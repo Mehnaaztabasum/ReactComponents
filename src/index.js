2de95f91190a53c8e4fd1a0cb30e8fb8aff469c6
Shaik Mehnaaz Tabasum <71725952+Mehnaaztabasum@users.noreply.github.com>
2022-08-26 14:51:44 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading"> my name</h1>
+    <ul>
+      <li> my fav food is</li>
+      <li> mehnaaz</li>
+      <li> tabasum</li>
+      <li> shaik</li>
+      <li>itsokays</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//render will take only one html element
